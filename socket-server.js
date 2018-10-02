3a4baebb64ddf2cc7de56ea7264caeda732a5b0d
edpantzar@gmail.com <edpantzar@gmail.com>
2018-10-02 13:22:43 -0500

    Logic sets db path

--- socket-server.js
+++ socket-server.js
@@ -5,6 +5,11 @@ const io = require('socket.io')(http);
 const path = require('path');
 
 const port = process.env.PORT || 8000;
+const db = (
+  process.env.NODE_ENV === 'production' ? 
+  'https://lovel-up.herokuapp.com' : 
+  'http://localhost:5000/'
+  );
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
