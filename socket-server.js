99c9033a504489c7fc20f24f28e163674a1da004
edpantzar@gmail.com <edpantzar@gmail.com>
2018-10-02 12:59:14 -0500

    Socket emits single message objects

--- socket-server.js
+++ socket-server.js
@@ -8,21 +8,13 @@ const port = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-let chat_history = [];
-//bring messages from database (knex)
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
   socket.on('disconnect', function () {
-    console.log('user disconnected');
   })
 
-  socket.emit('chat message', chat_history);
-  console.log(chat_history);
-
   socket.on('chat message', function (msg) {
-    chat_history.push(msg);
-    io.emit('chat message', chat_history);
+    io.emit('chat message', msg);
   });
 });
 
